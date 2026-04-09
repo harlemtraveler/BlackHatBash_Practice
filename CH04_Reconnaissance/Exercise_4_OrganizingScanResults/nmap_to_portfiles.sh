6514286dcd5c1f1d7d5179d47ee83fbedcb454a7
Tim <tjk315@gmail.com>
2026-04-09 21:16:23 +0700

    Correct typos in script

--- CH04_Reconnaissance/Exercise_4_OrganizingScanResults/nmap_to_portfiles.sh
+++ CH04_Reconnaissance/Exercise_4_OrganizingScanResults/nmap_to_portfiles.sh
@@ -8,8 +8,8 @@ while read -r line; do
 	if echo "${line}" | grep -q "report for"; then
 		ip=$(echo "${line}" | awk -F'for ' '{print $2}')
 	else
-		port=$(echo ${line} | grep "open" | awk -F'/' '{print $1}')
+		port=$(echo ${line} | grep open | awk -F'/' '{print $1}')
 		file="port-${port}.txt"
 		echo "${ip}" >> "${file}"
 	fi
-done <<< "${HOSTS_FILE}"
+done <<< "${RESULT}"
